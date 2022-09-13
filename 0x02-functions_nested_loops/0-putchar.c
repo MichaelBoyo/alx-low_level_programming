a6cd9f0340bdde2f83aafacb25372a7bdd7783e9
MichaelBoyo <boyomichaelbidemi@gmail.com>
2022-09-13 16:44:39 -0700

    ok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - check the code
+ * main - just a printf code
  *
  * Return: Always 0.
  */
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+
+void print_alphabet()
+{
+	for(int x = 'A'; x <= 'Z'; x++)
+	{
+    		int lower_x = tolower(x);
+    		putchar(lower_x);
+	}
+}
